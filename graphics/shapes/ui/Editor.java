6c5fae900b6aec7067ad41b87772b34eea3c3beb
Kurz Fabien <kurzr.f@gmail.com>
2019-04-02 11:30:30 +0200

    Error handling improvement

--- graphics/shapes/ui/Editor.java
+++ graphics/shapes/ui/Editor.java
@@ -33,7 +33,7 @@ public class Editor extends JFrame
 		});
 		
 		this.buildModel();
-		this.sview = new ShapesView(null);
+		this.sview = new ShapesView(this.model);
 		this.sview.setPreferredSize(new Dimension(300,300));
 		this.getContentPane().add(this.sview, java.awt.BorderLayout.CENTER);
 	}
